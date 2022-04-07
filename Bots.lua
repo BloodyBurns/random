5a8ed8a18b01e83d5636945d3ce4a466c566da73
BloodyBurns <83672765+BloodyBurns@users.noreply.github.com>
2022-04-07 18:29:28 -0400

    Update Bots.lua

--- Bots.lua
+++ Bots.lua
@@ -1,3 +1,4 @@
 return {
-  
+  'emppu', 'gaga', 'whitekids', 'PlutoRuinedDollhouse', 'PlutoOn', 'grimsbloodissocool', 'ReportMeOrElse21', 'BeTeR_ThAnY0U123', 'Mimissbunny', 'hhfhfhfhhfggda', 'PackBibleLord', 'pop3benzos', 'gaga0', 'iiQKreen', 'SpokeToTheDeviI', 'lance', 'maytinq', 'uniqueminds3tW', 'uniqueminds3tALT', 'uniqueminds3t', 'uniqueminds3tAlt2',
+  'EGIRLSADDMEXD', 'H3LL4W4NT3D', 'iownubot2', 'newgenLOLOLOL', 'iownuloopbot1', 'iownuHAHAHAHA', 'exlampia', 'WhyteGirlsRules'
 }
