7fd3df5eb9e335aab1001f6dbd9f994fd5e2d307
BloodyBurns <83672765+BloodyBurns@users.noreply.github.com>
2022-04-12 01:17:28 -0400

    Update Bots.lua

--- Bots.lua
+++ Bots.lua
@@ -1,5 +1,5 @@
 return {
   'PlutoRuinedDollhouse', 'grimsbloodissocool', 'hhfhfhfhhfggda', 'ReportMeOrElse21', 'BeTeR_ThAnY0U123', 'iiQKreen', 'lance', 'uniqueminds3tW',
   'uniqueminds3tALT', 'uniqueminds3t', 'uniqueminds3tAlt2', 'EGIRLSADDMEXD', 'H3LL4W4NT3D', 'iownubot2', 'newgenLOLOLOL', 'iownuloopbot1', 'iownuHAHAHAHA',
-  'exlampia', 'WhyteGirlsRules', 'rileyswantedW', 'WhyteGirlsAreTheBest', 'BACKROOMSHROOMS'
+  'exlampia', 'WhyteGirlsRules', 'rileyswantedW', 'WhyteGirlsAreTheBest', 'BACKROOMSHROOMS', 'RileyWhipsBlacks'
 }
