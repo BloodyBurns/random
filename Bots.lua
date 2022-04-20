260f89cc2a6200c7fe9aab4f3136240136fe512a
BloodyBurns <83672765+BloodyBurns@users.noreply.github.com>
2022-04-20 01:04:24 -0400

    Update Bots.lua

--- Bots.lua
+++ Bots.lua
@@ -2,5 +2,5 @@ return {
   'PlutoRuinedDollhouse', 'grimsbloodissocool', 'hhfhfhfhhfggda', 'ReportMeOrElse21', 'BeTeR_ThAnY0U123', 'iiQKreen', 'lance', 'uniqueminds3tW',
   'uniqueminds3tALT', 'uniqueminds3t', 'uniqueminds3tAlt2', 'EGIRLSADDMEXD', 'H3LL4W4NT3D', 'iownubot2', 'newgenLOLOLOL', 'iownuloopbot1', 'iownuHAHAHAHA',
   'exlampia', 'WhyteGirlsRules', 'rileyswantedW', 'WhyteGirlsAreTheBest', 'BACKROOMSHROOMS', 'RileyWhipsBlacks', 'MurderThatBlackNig', 'exolifi3d4131', 'alghtnca',
-  'PIutoRuinedDoIlhouse', 'CageTheBlacksPeople', '3O6am', '1700sWereTheBest', 'TheClanKilledANiegr', 'BlacksNeedaBeHanged'
+  'PIutoRuinedDoIlhouse', 'CageTheBlacksPeople', '3O6am', '1700sWereTheBest', 'TheClanKilledANiegr', 'BlacksNeedaBeHanged', 'ShootDemNiegrs', 'KuKlexClanMemberBot'
 }
