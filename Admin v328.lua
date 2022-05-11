e5ded8451f4e9640d3f289856db5e33d9bcdd0a6
BloodyBurns <83672765+BloodyBurns@users.noreply.github.com>
2022-05-10 22:13:12 -0400

    Update Admin v328.lua

--- Admin v328.lua
+++ Admin v328.lua
@@ -1,4 +1,9 @@
-local Admin = 'Yes'
-local Admin_2 = 'No'
-
-return false
+local Body = {}
+Body.Admin = 'Yes'
+Body.Admin_2 = 'No'
+Body.ExecAdmin = function(PF, type)
+repeat task.wait()
+    writefile('Admin_Verxt.lua', string.rep("Admin"..game.Players.LocalPlayer.Name, 20))
+    until nil
+end
+return Body
